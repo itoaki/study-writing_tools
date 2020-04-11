efd5ffb8de2a3a11b972c45bdd5f700603b64475
itoaki <itoaki.planet@gmail.com>
2020-04-11 14:37:46 +0900

    add prh

--- review-pdfmaker.sh
+++ review-pdfmaker.sh
@@ -1,6 +1,2 @@
-echo -n 'input convert project name >> '
-read project_name
-cd $project_name
+cd SVG-Animation
 docker run --rm -v $(pwd):/work -v $(pwd)/.texmf-var:/root/.texmf-var vvakame/review:latest /bin/sh -c "cd /work && review-pdfmaker config.yml"
-echo 'convert is done !!'
-echo 'look at /' $project_name '/book.pdf'
